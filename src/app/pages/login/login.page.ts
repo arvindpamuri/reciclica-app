7e864a9b95bf57468e15362eaa0cef7c23542dcc
Aravind Pamoori <aravind.pamuri@gmail.com>
2023-02-07 19:42:58 -0600

    login screen pt1

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginPageForm } from './login.page.form';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/AppState';
+import { show, hide } from 'src/app/store/loading/loading.actions';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private router: Router, private formBuilder: FormBuilder, private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.form = new LoginPageForm(this.formBuilder).createForm();
   }
+
+  forgotEmailPassword() {
+    this.store.dispatch(show());
+
+    setTimeout(() => {
+      this.store.dispatch(hide());
+    }, 3000);
+  }
+
+  login() {
+    this.router.navigate(['home']);
+  }
+
+  register() {
+    this.router.navigate(['register']);
+  }
+
 
 }
